7b9d3d25fcf9524a826d5ac3a5e90a384378f150
unknown <you@example.com>
2023-06-04 15:29:56 +0500

    All files

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        int amount = 13676;
+        int bonus = amount / 20;
+
+        if (bonus < 1) {
+            bonus = 1;
+        }
+
+        System.out.println("количество миль:" + bonus);
+    }
+}
